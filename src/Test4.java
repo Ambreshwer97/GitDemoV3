5ec0c9ad73b2e24a24feaad8ebf171e9b777ebd1
surya <surya@LAPTOP-36J5KNKJ>
2025-01-19 18:50:37 +0530

    TE2 1st c..

---
+++ src/Test4.java
@@ -0,0 +1,14 @@
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("TL");
+		System.out.println("TE1");
+		System.out.println("TE1");
+		System.out.println("TE2");
+
+	}
+
+}
