22322796a191441600ac7cf8e28df7ee8a356d60
surya <surya@LAPTOP-36J5KNKJ>
2025-01-19 18:44:01 +0530

    TE 2 nd C....

---
+++ src/Test3.java
@@ -0,0 +1,12 @@
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("TL");
+		System.out.println("TE1");
+		System.out.println("TE1");
+	}
+
+}
